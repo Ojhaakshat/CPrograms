8cdad0e0919c7c210a15796ecd4c373d2874ce5d
Ojhaakshat <54197918+Ojhaakshat@users.noreply.github.com>
2020-09-10 13:09:42 +0530

    Update powerfunctions.cpp

--- powerfunctions.cpp
+++ powerfunctions.cpp
@@ -12,21 +12,25 @@ int power(int n,int p){
 	return powern;
 }
 
-int powerBetter(int n,int p){
-	if(p==0){
-		return 1;
-	}
+int binaryExponentiation(int x,int n)
+{
+    if(n==0)
+        return 1;
+    else if(n%2 == 0)        //n is even
+        return binaryExponentiation(x*x,n/2);
+    else                             //n is odd
+        return x*binaryExponentiation(x*x,(n-1)/2);
+}
+//With Modulo(for big numbers)
+int modularExponentiation(int x,int n,int M)
+{
+    if(n==0)
+        return 1;
+    else if(n%2 == 0)        //n is even
+        return modularExponentiation((x*x)%M,n/2,M);
+    else                             //n is odd
+        return (x*modularExponentiation((x*x)%M,(n-1)/2,M))%M;
 
-	int powernby2 = powerBetter(n,p/2);
-
-	int powern=1;
-	if(p&1){
-		powern = powernby2*powernby2*n;
-	}else{
-		powern = powernby2*powernby2;
-	}
-
-	return powern;
 }
 
 // 3 approach
